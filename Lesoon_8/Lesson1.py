0aa4d5de4b3d9aecf0ce913f17946e3a2f339f22
Denis <denis.andreev@cti.ru>
2020-12-31 15:18:30 +0300

    completed task

--- Lesoon_8/Lesson1.py
+++ Lesoon_8/Lesson1.py
@@ -1,10 +1,11 @@
 import json
-import xml
+import xml.etree.ElementTree as ET
 import  os
-from pprint import pprint
 import collections
 
 RESOURCE_DIR = 'resources//'
+XML_SAMPLE_FILE_NAME = 'newsafr.xml'
+JSON_SAMPLE_FILE_NAME = 'newsafr.json'
 
 class FileReader():
     __words_len_filter = 6
@@ -14,14 +15,22 @@ class FileReader():
 
     def read_json(self):
         deserialized_json = self.__load_json()
-        most_popular_word = self.__get_most_popular_theme_word(deserialized_json)
-        print(most_popular_word)
+        return self.__get_most_popular_theme_word(deserialized_json, parse_mode='json')
 
-    def __get_most_popular_theme_word(self, data_dict):
-        theme_title_list = [
-            item['title'].split()
-            for item in data_dict['rss']['channel']['items']
-        ]
+    def __get_most_popular_theme_word(self, data, parse_mode=None):
+        if parse_mode == 'json':
+            theme_title_list = [
+                item['title'].split()
+                for item in data['rss']['channel']['items']
+            ]
+        elif parse_mode == 'xml':
+            root = data.getroot()
+            theme_title_list = [
+                news.text.split()
+                for news in root.iter('title')
+                ]
+        else:
+            return None
         theme_words_list = [
             word
             for title in theme_title_list
@@ -36,14 +45,25 @@ class FileReader():
             for word in theme_words_list
         ]
 
-    def __read_xml(self):
-        pass
+    def read_xml(self):
+        tree = self.__load_xml()
+        return self.__get_most_popular_theme_word(tree, parse_mode='xml')
 
 
     def __load_json(self):
         with open(self.file_path, encoding='utf-8') as fp:
             return json.load(fp)
 
-ff = FileReader(os.path.join(RESOURCE_DIR, 'newsafr.json'))
+    def __load_xml(self):
+        return ET.parse(self.file_path)
 
-ff.read_json()
+if __name__ == '__main__':
+    most_popular_json_words = FileReader(os.path.join(RESOURCE_DIR, JSON_SAMPLE_FILE_NAME))
+    most_popular_json_words = most_popular_json_words.read_json()
+    print('Json result:\n', most_popular_json_words)
+
+    print()
+
+    most_popular_xml_words = FileReader(os.path.join(RESOURCE_DIR, XML_SAMPLE_FILE_NAME))
+    most_popular_xml_words = most_popular_xml_words.read_xml()
+    print('XML result:\n', most_popular_xml_words)
